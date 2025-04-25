ebdabd4929f166f7ce753168557de72b30618d0b
Guillaumeperrottet <perrottet.guillaume.97@gmail.com>
2025-04-25 11:10:44 +0200

    Barre de navigation et Navbar problème réglé

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import Navbar from "./components/Navbar";
 import { getUser } from "../lib/auth-session";
 import "./globals.css";
 import TodoListAgendaWrapper from "./components/TodoListAgendaWrapper";
-import { headers } from "next/headers";
 
 export default async function RootLayout({
   children,
@@ -11,19 +10,12 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   const user = await getUser();
-  const pathname =
-    headers().get("x-invoke-path") || headers().get("next-url") || "";
-
-  const isLanding = pathname === "/" || pathname === "";
 
   return (
     <html lang="en">
       <body>
-        {!isLanding && <Navbar user={user} />}
-        <div className="pb-16 md:pb-14">
-          {/* Ajouter un padding en bas pour éviter que le contenu soit caché par l'agenda */}
-          {children}
-        </div>
+        {user && <Navbar user={user} />}
+        <div className="pb-16 md:pb-14">{children}</div>
         {user && <TodoListAgendaWrapper />}
         <Toaster position="top-center" richColors />
       </body>
