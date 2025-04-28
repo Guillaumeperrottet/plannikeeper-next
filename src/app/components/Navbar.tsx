8580a2f5d3e8b17712d956bfd4b6eaa993dd34d0
Guillaumeperrottet <perrottet.guillaume.97@gmail.com>
2025-04-28 09:15:44 +0200

    navbar dark mode fonctionnel

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,9 +1,7 @@
 "use client";
 
 import Link from "next/link";
-
 import { VT323 } from "next/font/google";
-
 import Switch from "@/app/components/ui/switchmode";
 
 const vt323 = VT323({
@@ -14,10 +12,20 @@ const vt323 = VT323({
 
 export default function Navbar() {
   return (
-    <nav className="w-full bg-background border-b border-border px-4 py-2 flex justify-between items-center relative z-50">
+    <nav
+      className="w-full border-b px-4 py-2 flex justify-between items-center relative z-50"
+      style={{
+        background: "var(--background)",
+        borderColor: "var(--border)",
+        transition: "background 0.3s, color 0.3s",
+      }}
+    >
       <Link
         href="/dashboard"
-        className={`text-4xl font-bold ${vt323.className} text-primary-foreground`}
+        className={`text-4xl font-bold text-[color:var(--foreground)] ${vt323.className}`}
+        style={{
+          transition: "color 0.3s",
+        }}
       >
         PlanniKeeper
       </Link>
