a3634b1379dee86a56d2254351539403ffb3df59
Guillaumeperrottet <perrottet.guillaume.97@gmail.com>
2025-04-29 14:48:11 +0200

    fix deploy errors

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,14 +1,14 @@
-import { createAuthClient } from "better-auth/react"
+import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000",
+  /** The base URL of the server (optional if you're using the same domain) */
+  baseURL: "http://localhost:3000",
 
-     // Options supplémentaires pour le débogage
-     options: {
-      debugging: true,
-      onError: (error) => {
-          console.error("Auth Client Error:", error);
-      }
-  }
-})
+  // Options supplémentaires pour le débogage
+  options: {
+    debugging: true,
+    onError: (error: unknown) => {
+      console.error("Auth Client Error:", error);
+    },
+  },
+});
