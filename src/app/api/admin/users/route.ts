23ec5bbec76c3d45e351769f0650f090dffaa68d
Guillaumeperrottet <perrottet.guillaume.97@gmail.com>
2025-05-22 13:52:03 +0200

    ajouter user depuis superadmin

--- src/app/api/admin/users/route.ts
+++ src/app/api/admin/users/route.ts
@@ -76,7 +76,6 @@ export async function POST(request: NextRequest) {
         { status: 403 }
       );
     }
-    r;
 
     // Récupérer les données du nouvel utilisateur
     const userData = await request.json();
